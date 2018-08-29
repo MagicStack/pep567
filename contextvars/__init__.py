ab5321546ca3917399c88ea00d14997b1fbe8521
Fantix King <fantix.king@gmail.com>
2018-08-29 18:41:55 +0800

    Patch loops to copy context on task creation.

--- contextvars/__init__.py
+++ contextvars/__init__.py
@@ -1,6 +1,7 @@
 import asyncio
 import collections.abc
 import threading
+import types
 
 import immutables
 
@@ -209,3 +210,39 @@ def _get_state():
 
 
 _state = threading.local()
+
+
+def create_task(loop, coro):
+    task = loop._orig_create_task(coro)
+    if task._source_traceback:
+        del task._source_traceback[-1]
+    task.context = copy_context()
+    return task
+
+
+def _patch_loop(loop):
+    if loop and not hasattr(loop, '_orig_create_task'):
+        loop._orig_create_task = loop.create_task
+        loop.create_task = types.MethodType(create_task, loop)
+    return loop
+
+
+def get_event_loop():
+    return _patch_loop(_get_event_loop())
+
+
+def set_event_loop(loop):
+    return _set_event_loop(_patch_loop(loop))
+
+
+def new_event_loop():
+    return _patch_loop(_new_event_loop())
+
+
+_get_event_loop = asyncio.get_event_loop
+_set_event_loop = asyncio.set_event_loop
+_new_event_loop = asyncio.new_event_loop
+
+asyncio.get_event_loop = asyncio.events.get_event_loop = get_event_loop
+asyncio.set_event_loop = asyncio.events.set_event_loop = set_event_loop
+asyncio.new_event_loop = asyncio.events.new_event_loop = new_event_loop
